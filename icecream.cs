214bcd0447ff66a9b2a972459fa07c22e3dc9492
Ben Ngapo <benngapo@mmc.school.nz>
2022-09-28 13:23:19 +1300

    ading ice

---
+++ icecream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SleepyJoe
+{
+    class Icecream
+    {
+        public int x, y, width, height; // rectangle variables
+        public Image iceImage; //variable for stair image
+
+        public Rectangle iceRec; //variable for a rectangle to place the image in
+
+
+        //Create a constructor (initialises the values of the fields)
+        public Icecream(int spacing)
+        {
+            x = -spacing;
+            y = 10;
+            width = 200;
+            height = 200;
+            //stairImage contains the stair.png image
+            iceImage = Properties.Resources.iceream;
+            iceRec = new Rectangle(x, y, width, height);
+        }
+
+        public void DrawIce(Graphics g)
+        {
+            iceRec = new Rectangle(x, y, width, height);
+            g.DrawImage(iceImage, iceRec);
+        }
+        public void MoveStairs()
+        {
+            y += 5;
+            iceRec.Location = new Point(x, y);
+        }
+    }
+
+}
